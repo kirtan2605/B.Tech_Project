371ada5cbb0010400ea452ec012222c50b0ed8e2
Kirtan Patel <59549172+kirtan2605@users.noreply.github.com>
2023-01-19 13:15:05 +0530

    Delete BTechProject_v1 directory

--- BTechProject_v1/PDF_Control.py
+++
@@ -1,8 +0,0 @@
-def PDF_Control_Dougherty(roll_error, roll_error_rate):
-    # initializing Control variables as per Dougherty1968
-    Kphi = 62.3676  # Control parameter in joules/rad
-    tau = 25  # time constant of controller zero in sec
-
-    Control_Torque = Kphi * (tau * roll_error_rate + roll_error)
-
-    return Control_Torque
--- BTechProject_v1/disturbance_torque.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def disturbance_torques(steps):
-    Tdx_model = np.zeros(steps)
-    Tdz_model = np.zeros(steps)
-
-    return Tdx_model, Tdz_model
