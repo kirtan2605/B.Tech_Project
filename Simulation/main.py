206abbee5bc6223a34e13b6ef4c5be0da35ffcac
Kirtan RHEL9 <kirtan.2791@gmail.com>
2023-02-19 00:38:34 +0530

    Virtual Environment Set-up

--- Simulation/main.py
+++ Simulation/main.py
@@ -12,13 +12,11 @@ runtime_parameters = [start_time, end_time, time_step]
 MOI_x = 1475.8425                   # moment of inertia about x-axis in kg.m^2
 MOI_y = 886.2425                    # moment of inertia about x-axis in kg.m^2
 MOI_z = 1532.96                     # moment of inertia about x-axis in kg.m^2
-wheel_momentum = 11                  # angular momentum of momentum wheel in N.m.sec
+wheel_momentum = 11                 # angular momentum of momentum wheel in N.m.sec
 orbit_rate = 2*math.pi/86400        # orbit rate in rad/sec
 
 nutation_frequency = wheel_momentum/((MOI_x*MOI_z)**0.5)
 system_variables = [MOI_x, MOI_y, MOI_z, orbit_rate, nutation_frequency, wheel_momentum]
-
-# considering the system to be under
 
 # initializing initial value of state variables
 initial_roll = 0.0174533        # initial roll angle in radians
@@ -27,10 +25,8 @@ initial_roll_rate = 0           # initial roll rate in radians/sec
 initial_yaw_rate = 0            # initial yaw rate in radians/sec
 initial_conditions = [initial_roll, initial_roll_rate, initial_yaw, initial_yaw_rate]
 
-# initial condition on torques are specified in run_simulation.py
-
 # initializing System Control variables
-roll_desired = 0          # desired roll angle in radians (5 deg = 0.0872665)
-offset_angle = 35.25          # offset angle of thruster in degree
+roll_desired = radians(2)       # desired roll angle in radians (5 deg = 0.0872665)
+offset_angle = 35.25            # offset angle of thruster in degree
 
 run_simulation(roll_desired, offset_angle, runtime_parameters, system_variables, initial_conditions)
