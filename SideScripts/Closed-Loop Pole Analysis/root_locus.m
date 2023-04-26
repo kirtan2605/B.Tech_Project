e2aacf134e2698567a359566482eedde8e1888df
Kirtan Patel <59549172+kirtan2605@users.noreply.github.com>
2023-04-26 21:49:26 +0530

    Update root_locus.m

--- SideScripts/Closed-Loop Pole Analysis/root_locus.m
+++ SideScripts/Closed-Loop Pole Analysis/root_locus.m
@@ -1,6 +1,6 @@
-Ix = 1000;
-Iz = Ix ;
-h = 150;
+Ix = 1475.8425 ;
+Iz = 1532.9 ;
+h = 20;
 
 omega_0 = 0.72722*(10^-4) ;
 
@@ -17,12 +17,77 @@ s = tf('s');
 H_roll = cosd ( alpha_d ) *( Ix * s ^2 + h * tand ( alpha_d ) * s + omega_0 *h ) /( Ix * Iz *( s ^2 + nutation_f_approx ^2) *( s ^2 + omega_0 ^2 ) ) ;
 
 figure(1)
-rlocus ( H_roll ) , grid
+rlocus ( H_roll ), grid
+xlim([-0.002 0.0001])
+ylim([-0.0005 0.0005])
+%set(gca,'XTickLabel',[0.0002 0.0001 0]);
+%set(gca,'YTickLabel',[1.5 1.45 1.4 1.35 1.3 1.25]);
+zeta = [0.99 0.975  0.95 0.9 0.85 0.75 0.5 0.25 0];
+wn = [0.0025 0.0015 0.001 0.0005 0];
+sgrid(zeta,wn);
+title('Orbit Rate Poles Root Locus')
+saveas(gcf,'RL_01_OrbitPoles.png')
 
 figure(2)
-G = ( tau * s - 1) ;
-rlocus ( G * H_roll ) , grid
+rlocus ( H_roll ), grid
+xlim([-0.0005 0.0025])
+ylim([-0.04 0.04])
+%set(gca,'XTickLabel',[0.0002 0.0001 0]);
+%set(gca,'YTickLabel',[1.5 1.45 1.4 1.35 1.3 1.25]);
+zeta = [0.05 0.005 0];
+wn = [0.1 0.05 0.03 0];
+sgrid(zeta,wn);
+title('Nutation Poles Root Locus')
+saveas(gcf,'RL_01_NutationPoles.png')
 
 figure(3)
+G = ( tau * s - 1) ;
+rlocus ( G * H_roll ) , grid
+xlim([-0.001 0.0005])
+ylim([-0.0002 0.0002])
+%set(gca,'XTickLabel',[0.0002 0.0001 0]);
+%set(gca,'YTickLabel',[1.5 1.45 1.4 1.35 1.3 1.25]);
+zeta = [0.99 0.975  0.95 0.9 0.5 0];
+wn = [0.0025 0.0015 0.001 0.0005 0];
+sgrid(zeta,wn);
+title('Orbit Rate Poles Root Locus')
+saveas(gcf,'RL_02_OrbitPoles.png')
+
+figure(4)
+G = ( tau * s - 1) ;
+rlocus ( G * H_roll ) , grid
+xlim([-0.025 0.00001])
+ylim([-0.02 0.02])
+%set(gca,'XTickLabel',[0.0002 0.0001 0]);
+%set(gca,'YTickLabel',[1.5 1.45 1.4 1.35 1.3 1.25]);
+zeta = [0.95 0.5 0.1 0];
+wn = [0.05 0.03 0.02 0.01 0.005 0];
+sgrid(zeta,wn);
+title('Nutation Poles Root Locus')
+saveas(gcf,'RL_02_NutationPoles.png')
+
+figure(5)
 G = ( tau * s + 1) ;
 rlocus ( G * H_roll ) , grid
+xlim([-0.002 0.00005])
+ylim([-0.002 0.002])
+%set(gca,'XTickLabel',[0.0002 0.0001 0]);
+%set(gca,'YTickLabel',[1.5 1.45 1.4 1.35 1.3 1.25]);
+zeta = [0.95 0.75 0.5 0.1 0];
+wn = [0.0025 0.0015 0.001 0.0005 0];
+sgrid(zeta,wn);
+title('Orbit Rate Poles Root Locus')
+saveas(gcf,'RL_03_OrbitPoles.png')
+
+figure(6)
+G = ( tau * s + 1) ;
+rlocus ( G * H_roll ) , grid
+xlim([-0.2 0.0001])
+ylim([-0.1 0.1])
+%set(gca,'XTickLabel',[0.0002 0.0001 0]);
+%set(gca,'YTickLabel',[1.5 1.45 1.4 1.35 1.3 1.25]);
+zeta = [0.95 0.5 0.1 0];
+wn = [0.25 0.2 0.15 0.1 0.05 0];
+sgrid(zeta,wn);
+title('Nutation Poles Root Locus')
+saveas(gcf,'RL_03_NutationPoles.png')
