5857570a5011b4f683e04a99c9ed56c6a16082c5
Kirtan Patel <59549172+kirtan2605@users.noreply.github.com>
2023-04-26 14:52:05 +0530

    Update root_locus.m

--- SideScripts/Closed-Loop Pole Analysis/root_locus.m
+++ SideScripts/Closed-Loop Pole Analysis/root_locus.m
@@ -1,44 +1,28 @@
 Ix = 1000;
-Iz = Ix;
+Iz = Ix ;
 h = 150;
 
-nutation_f = h/(Ix*Iz)^0.5;
-omega_0 = 0.727*(10e-4);
-%disp(nutation_f);
-%disp(omega_0);
-disp(['     Nutation Frequency / Omega_0 =', num2str(nutation_f/omega_0)]);
-% h = k (pitch angular momentum) = k(Iy*omega_0);
-% for geostationarity of the satellite oemga_0 is nearly 7.2711e-5
+omega_0 = 0.72722*(10^-4) ;
 
+% nutation frequency for large h
+nutation_f_approx = h /(( Ix * Iz ) ^0.5) ;
+
+K = 15;
 tau = 25;
-alpha_d = 10;
+alpha_d = 10; % alpha in degrees
 
-nr3 = tau*Ix;
-nr2 = (tau*h*tand(alpha_d) + Ix);
-nr1 = (tau*omega_0*h + h*tand(alpha_d));
-nr0 = omega_0*h;
-nr = [nr3 nr2 nr1 nr0];
+s = tf('s');
 
-dr4 = Ix*Iz;
-dr3 = 0;
-dr2 = Ix*Iz*(omega_0^2) + h*h;
-dr1 = 0;
-dr0 = h^2 * omega_0^2;
-dr = [dr4 dr3 dr2 dr1 dr0];
+% H is used to denote the transfer function of the plant .
+H_roll = cosd ( alpha_d ) *( Ix * s ^2 + h * tand ( alpha_d ) * s + omega_0 *h ) /( Ix * Iz *( s ^2 + nutation_f_approx ^2) *( s ^2 + omega_0 ^2 ) ) ;
 
+figure(1)
+rlocus ( H_roll ) , grid
 
-% realistic K
-% 1 + K.H(s) where H(s) = a(s)/b(s) --> b(s) + K.a(s) = 0
-K = 0:1:1000;
-n = length(K);
-p = zeros([4 n]);
-for i = 1:n
-    ch_eq = [dr4 (dr3+K(i)*nr3) (dr2+K(i)*nr2) (dr1+K(i)*nr1) (dr0+K(i)*nr0)];
-    p(:,i) = roots(ch_eq);
-end
-plot(real(p), imag(p), '.'),grid
-xlabel('real');
-ylabel('imaginery');
+figure(2)
+G = ( tau * s - 1) ;
+rlocus ( G * H_roll ) , grid
 
-
-%rlocus(nr,dr),grid
+figure(3)
+G = ( tau * s + 1) ;
+rlocus ( G * H_roll ) , grid
